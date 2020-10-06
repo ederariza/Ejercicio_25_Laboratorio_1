ed3a270b9f4923a82f2e51b2b92a3fd6d928d10e
Infromatica II <eder.ariza@udea.edu.co>
2020-10-06 02:53:47 -0500

    Ejercicio 25 Laboratorio 1

---
+++ main.cpp
@@ -0,0 +1,17 @@
+
+/*
+Ejercicio 25. Escriba un programa que pida un número N e imprima en pantalla la cantidad de
+dígitos de N.
+4Ej: si se ingresa 1234 se debe imprimir:
+1234 tiene 4 digitos.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
